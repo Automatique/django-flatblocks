7d462443b29c0c9f253fb6c814eee0e418a15ff5
Tijs Teulings <tijs@automatique.nl>
2012-08-14 15:12:58 +0200

    added tinymce for heade too

--- flatblocks/forms.py
+++ flatblocks/forms.py
@@ -4,6 +4,7 @@ from flatblocks.models import FlatBlock
 from tinymce.widgets import TinyMCE
 
 class FlatBlockForm(forms.ModelForm):
+    header = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 4}))
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 16}))
 
     class Meta:
