dbecce132ce3c5f5ac23bc9da4fa7850f34cbb47
Tijs Teulings <tijs@automatique.nl>
2012-08-14 15:24:58 +0200

    forgot to reference adminform in admin.py

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from flatblocks.models import FlatBlock
-
+from flatblocks.forms import FlatBlockAdminForm
 
 class FlatBlockAdmin(admin.ModelAdmin):
+    form = FlatBlockAdminForm
+
     ordering = ['slug', ]
     list_display = ('slug', 'header')
     search_fields = ('slug', 'header', 'content')
--- flatblocks/forms.py
+++ flatblocks/forms.py
@@ -4,6 +4,11 @@ from flatblocks.models import FlatBlock
 from tinymce.widgets import TinyMCE
 
 class FlatBlockForm(forms.ModelForm):
+    class Meta:
+        model = FlatBlock
+        exclude = ('slug', )
+
+class FlatBlockAdminForm(forms.ModelForm):
     header = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 4}))
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 16}))
 
